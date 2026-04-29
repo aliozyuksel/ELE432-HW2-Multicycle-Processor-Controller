4abc0431aeaf7e34437ff92b3b7fc19664b1a417
Ali Özyüksel <80827423+aliozyuksel@users.noreply.github.com>
2026-04-29 19:32:37 +0300

    Add files via upload

---
+++ aludec.sv
@@ -0,0 +1,31 @@
+module aludec (
+    input  logic       opb5,
+    input  logic [2:0] funct3,
+    input  logic       funct7b5,
+    input  logic [1:0] ALUOp,
+    output logic [2:0] ALUControl
+);
+
+    logic RtypeSub;
+    assign RtypeSub = funct7b5 & opb5;
+
+    always_comb begin
+        case (ALUOp)
+            2'b00: ALUControl = 3'b010; 
+            2'b01: ALUControl = 3'b110; 
+            
+            default: case (funct3)      
+                3'b000: begin
+                    if (RtypeSub)
+                        ALUControl = 3'b110; 
+                    else
+                        ALUControl = 3'b010; 
+                end
+                3'b010: ALUControl = 3'b111; 
+                3'b110: ALUControl = 3'b001; 
+                3'b111: ALUControl = 3'b000;
+                default: ALUControl = 3'b000;
+            endcase
+        endcase
+    end
+endmodule
---
+++ controller.sv
@@ -0,0 +1,52 @@
+module controller (
+    input  logic       clk,
+    input  logic       reset,
+    input  logic [6:0] op,
+    input  logic [2:0] funct3,
+    input  logic       funct7b5,
+    input  logic       zero,
+    output logic [1:0] immsrc,
+    output logic [1:0] alusrca, alusrcb,
+    output logic [1:0] resultsrc,
+    output logic       adrsrc,
+    output logic [2:0] alucontrol,
+    output logic       irwrite, pcwrite,
+    output logic       regwrite, memwrite
+);
+
+    logic [1:0] aluop;
+    logic       branch;
+    logic       pcupdate;
+
+    mainfsm mainfsm_inst (
+        .clk(clk),
+        .reset(reset),
+        .op(op),
+        .Branch(branch),
+        .PCUpdate(pcupdate),
+        .RegWrite(regwrite),
+        .MemWrite(memwrite),
+        .IRWrite(irwrite),
+        .ResultSrc(resultsrc),
+        .ALUSrcB(alusrcb),
+        .ALUSrcA(alusrca),
+        .AdrSrc(adrsrc),
+        .ALUOp(aluop)
+    );
+
+    aludec aludec_inst (
+        .opb5(op[5]),          
+        .funct3(funct3),
+        .funct7b5(funct7b5),
+        .ALUOp(aluop),          
+        .ALUControl(alucontrol)
+    );
+
+    instrdec instrdec_inst (
+        .op(op),
+        .ImmSrc(immsrc)
+    );
+
+    assign pcwrite = pcupdate | (branch & zero);
+
+endmodule
